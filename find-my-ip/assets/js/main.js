172c80fac6ac84c604b87d984f748bdb9ce29a4a
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-22 15:40:21 +0530

    feat: find-my-ip-app

---
+++ find-my-ip/assets/js/main.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", loadAllcontent);
+
+function loadAllcontent() {
+  const api = "https://api.ipify.org?format=json";
+
+  fetch(api)
+    .then((data) => data.json())
+    .then((data) => {
+      const footertxt = document.querySelector("#footertxt");
+      console.log(data.ip);
+      footertxt.textContent = data.ip;
+    })
+    .catch((err) => {
+      console.log(err);
+      footertxt.textContent = "there is some errors while fetching datas";
+    });
+
+  const agreeBtn = document.querySelector("#agreeBtn");
+  const agreesection = document.querySelector("#agreesection");
+
+  agreeBtn.addEventListener("click", () => {
+    agreesection.classList.add("hidden");
+  });
+}
